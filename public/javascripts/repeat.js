4e7c61febfdba2835f057309421b925ebcf14b33
foudilredaoui <df_redaoui@esi.dz>
2018-04-17 11:03:13 +0100

    add the suggestios script

--- public/javascripts/repeat.js
+++ public/javascripts/repeat.js
@@ -18,10 +18,29 @@ var FormRepeater = function() {
             }   
         });
     }
+    var demo2 = function() {
+        $('#m_repeater_2').repeater({            
+            initEmpty: false,
+           
+            defaultValues: {
+                'text-input': 'foo'
+            },
+             
+            show: function() {
+                $(this).slideDown();                               
+            },
+
+            hide: function(deleteElement) {                 
+                    $(this).slideUp(deleteElement);
+                                               
+            }      
+        });
+    }
     return {
         // public functions
         init: function() {
             demo1();
+            demo2();
         }
     };
 }();
