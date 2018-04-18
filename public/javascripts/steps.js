44cb85b0a715e1701064ef7b3f3989716f7f046d
foudilredaoui <df_redaoui@esi.dz>
2018-04-18 14:27:46 +0100

    quizz js

--- public/javascripts/steps.js
+++ public/javascripts/steps.js
@@ -2,21 +2,40 @@ var stepper = function(){
     var qst1 = $('#qst1');
     var qst2 = $('#qst2');
     var qst3 = $('#qst3');
-    
+    var qsts = [qst1,qst2,qst3] ;
+    var nextBtn = $('#next');
+    var prevBtn = $('#prev');
+    var prog = $('.progress-bar');
+
+    var i =1; 
     qst1.show();
     qst2.hide();
     qst3.hide();
 
 var next = function(){
+nextBtn.click(function() {
+    qsts[i-1].hide();
+    qsts[i].show();
+    i++;
 
+  });
 
 }
-
+// var prev = function(){
+//     prevBtn.click(function() {
+//         qsts[i].hide();
+//         qsts[i-1].show();
+//         i--;
+    
+//       });
+    
+// }
 
 return {
     // public functions
     init: function() {
        next();
+       prev();
     }
 };
 
